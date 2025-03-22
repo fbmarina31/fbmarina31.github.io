f2f92e275249c29bca74d9eda2593049a44a94bd
fbmarina31 <fbmarina31@gmail.com>
2025-03-22 15:48:17 +0100

    Change to horizontal navigation bar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="q-pa-sm">
     <q-layout view="hHh Lpr lff">
-      <q-header :class="$q.dark.isActive ? 'bg-secondary' : 'bg-white'" style="height: 100px">
-        <q-toolbar class="q-pl-lg q-pb-sm" style="height: 100px">
+      <q-header :class="$q.dark.isActive ? 'bg-secondary' : 'bg-white'">
+        <q-toolbar class="q-pl-lg q-pb-sm">
           <a href="/">
             <img
               src="https://static.wixstatic.com/media/65c703_d6cca897e9b244f188ab20f8f5ac193f~mv2.png/v1/fill/w_124,h_100,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Untitled.png"
@@ -17,30 +17,13 @@
           <q-btn flat round dense icon="phone" color="grey-9" class="q-mt-xl q-pl-md" />
           <q-btn flat round dense icon="mail" color="grey-9" class="q-mt-xl q-pl-md" />
         </q-toolbar>
+        <q-tabs class="text-black">
+          <q-route-tab to="/home" label="Home" />
+          <q-route-tab to="/products" name="videos" label="Produkte" />
+          <q-route-tab to="/about" name="articles" label=" Über uns" />
+          <q-route-tab to="/contact" name="articles" label="Kontakt" />
+        </q-tabs>
       </q-header>
-      <q-drawer
-        v-model="drawer"
-        show-if-above
-        :width="200"
-        :breakpoint="500"
-        bordered
-        :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
-      >
-        <q-list>
-          <q-item to="/home" clickable>
-            <q-item-section> Home </q-item-section>
-          </q-item>
-          <q-item to="products" clickable>
-            <q-item-section> Produkte </q-item-section>
-          </q-item>
-          <q-item to="about" clickable>
-            <q-item-section> Über uns </q-item-section>
-          </q-item>
-          <q-item to="contact" clickable>
-            <q-item-section> Kontakt </q-item-section>
-          </q-item>
-        </q-list>
-      </q-drawer>
 
       <q-page-container>
         <router-view />
@@ -57,8 +40,4 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const drawer = ref(false);
-</script>
+<script setup lang="ts"></script>
