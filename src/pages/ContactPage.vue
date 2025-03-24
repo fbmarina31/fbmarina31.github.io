94a4462b04760fccd26fe662b9009c5fd7539edf
fbmarina31 <fbmarina31@gmail.com>
2025-03-24 22:26:33 +0100

    Edit Contact Page

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,5 +1,36 @@
 <template>
-  <q-page class="row items-center justify-evenly"> Contact Page </q-page>
+  <q-page> 
+    <div class="q-pa-md">
+      <div>
+    <div class="text-h2 q-pa-xs text-grey-9 text-center	">Kontakt</div>
+    <div class="row justify-center q-pb-xl q-mt-sm">
+      <q-btn class="flat dense no-shadow text-grey-9 text-subtitle1 q-pa-xs text-center "
+      icon="mail"
+      label="E-Mail schreiben" 
+      type="a" href="mailto:example@example.com"> </q-btn>
+    </div>
+  </div>
+    <div class="text-h2 q-pa-xs text-grey-9 text-center	">Impressum</div>
+    <div class="text-subtitle1 q-pa-xs text-grey-9 text-center">Raaf und Hochstatter GbR<br>
+Franziska Raaf und Fabienne Hochstatter<br>
+Pfarrgasse 6 73460 Hüttlingen<br>
+Telefon: +49 (0) 7975 9699993<br>
+E-Mail: lumiakreativ@gmail.com<br>
+Website: https://lumiakreativ.de<br>
+Vertretungsberechtigte/r Geschäftsführer/in: Fabienne Hochstatter und Franziska Raaf<br>
+Steuernummer: 50081/07151 </div>
+</div>
+<div class="q-pa-md">
+<div class="text-h2 q-pa-xs text-grey-9 text-center	">Datenschutz</div>
+<div class="row justify-center q-mt-lg text-grey-9 text-subtitle1">
+  <q-btn class="no-shadow text-grey-9 text-grey-9 text-subtitle1"
+  label="PDF herunterlagen" 
+  type="a" href="/logo/Datenschutzrichtlinie_LuMia_kreativ.pdf" 
+  download="Datenschutzrichtlinie_LuMia_kreativ.pdf" 
+  icon="cloud_download"/>
+</div>
+</div>
+</q-page>
 </template>
 
 <script setup lang="ts">
