37a4f94325858f72e06b8dad9b8ad4e3c3fd762c
fbmarina31 <fbmarina31@gmail.com>
2025-03-24 22:26:10 +0100

    Edit about us

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,7 +1,28 @@
 <template>
-  <q-page class="row items-center justify-evenly"> About Page </q-page>
+  <q-page class="rowjustify-center">
+    <div class="row justify-center">
+  <div class="q-pa-md" style="width: 600px">
+    <div class="text-h2 q-pa-xs text-grey-9 text-center	">Über uns <br> </div>
+    <div class="text-subtitle1 q-pa-xs text-grey-9 text-center	">Hinter LuMia stecken Fabienne und Franzi, wir lieben es neue Dinge auszuprobieren, handwerklich und kreativ tätig zu sein.<br> <br>
+Wir haben uns bei der Arbeit vor 6 Jahren kennen gelernt und schnell gemerkt, dass wir sehr viele Interessen und Hobbys teilen. <br> <br>
+Wir sind gerne kreativ und probieren neue Dinge aus. Hierzu gehört vor allem das betonieren von Dekoartikeln, aber auch vieles mehr. <br> <br>
+Oft haben wir bei der Arbeit darüber gescherzt und gemeint wir brauchen auch ein eigenes „Business“ wie wir es immer genannt haben. <br> <br>
+Im Juni 2022 haben wir endgültig beschlossen, dass es nun wirklich Zeit dazu wird unser Hobby mit euch zu teilen.<br> <br>
+So viel nun zu unserer kleinen Geschichte, jedoch bleibt nun noch eine Frage offen. <br> <br>
+Wie kam es zum Namen LuMia? <br> <br>
+Im April 2020 sind unsere beiden Kinder Luisa und Mia zur Welt gekommen. Diesen beiden Mädels möchten wir gerne den Namen LuMia widmen. </div>
+  </div>
+  <div>
+    <div class="q-pa-md q-ma-sm">
+      <img src=\logo\aboutus.png
+              class="q-mt-md q-pb-none"
+              style="width: 190px"
+            />
+  </div>
+  </div>
+  </div> 
+  </q-page>
 </template>
-
 <script setup lang="ts">
 
 </script>
